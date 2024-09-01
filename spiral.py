8af3eec4765f40b282e8eba54b5ca59c99c6aadb
trinityvthompson <trinityvthompson@utexas.edu>
2024-09-01 15:42:55 -0500

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -30,13 +30,47 @@ def sum_adjacent_numbers(spiral, n):
 
 import math
 
-
 def create_spiral(dim):
     """Creates a Spiral given a dimension for the spiral dimeter"""
 
-    matrix = [[0] * dim for _ in range(dim)]
-  
+    spiral = [[0] * dim for i in range(dim)]
 
+    # Set starting point for the spiral as center of grid
+    x = dim // 2
+    y = dim // 2
+
+    # Center is 1
+    spiral[x][y] = 1
+
+    num = 2
+    step = 1
+
+    while num <= dim**2: 
+        for i in range(step):
+            y += 1 # Move right 
+            spiral[x][y] = num
+            num += 1
+        
+        for i in range(step):
+            x += 1 # Move down
+            spiral[x][y] = num
+            num += 1
+    
+        step += 1
+
+        for i in range(step):
+            y -= 1 # Move left
+            spiral[x][y] = num
+            num += 1
+
+        for i in range(step):
+            x -= 1 # Move up 
+            spiral[x][y] = num
+            num += 1
+
+        step += 1
+
+    return spiral 
 
 
 def sum_sub_grid(grid, val):
@@ -49,11 +83,18 @@ def sum_sub_grid(grid, val):
     surrounding the parameter val in the grid
     if val is out of bounds, returns 0
     """
-    
-    # ADD YOUR CODE HERE  
-
-
-    return # ADD YOUR CODE HERE  
+    sum = 0
+    for x in range(len(grid)):
+        for y in range(len(grid)):
+            if grid[x][y] == val:
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        sum += grid[x + i][y + j]
+                sum -= val
+                return sum
+                # sum = (grid[x][y-1]) + (grid[x+1][y]) + (grid[x-1][y]) + (grid[x][y+1]) 
+            # else:
+            #     return 0
 
 
 
@@ -90,3 +131,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+  
